aa98b59388d212f3f6764c811c882fb5029dce15
xflomasterx <82092585+xflomasterx@users.noreply.github.com>
2021-04-07 15:33:03 +0300

    init commit

---
+++ Assets/controller.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float torque;
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+    float turn;
+    public float torqueCurrent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        torqueCurrent = torque;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (torqueCurrent > 0)
+        {
+            torqueCurrent -= 0.01f;
+            rb.AddTorque(transform.right * torqueCurrent * turn, ForceMode.Force);
+        }
+        
+    }
+    void Update()
+    {
+        turn = Swipe();
+    }
+    public float Swipe()
+    {
+        //for PC debug
+        /*
+        if (Input.GetMouseButtonDown(0))
+        {
+            //save began touch 2d point
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            secondPressPos = firstPressPos;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //save ended touch 2d point
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            torqueCurrent = torque;
+        }*/
+        if (Input.touchCount > 0) {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                //save began touch 2d point
+                firstPressPos = new Vector2(touch.position.x, touch.position.y);
+                secondPressPos = firstPressPos;
+            }
+            if (touch.phase == TouchPhase.Ended)
+            {
+                //save ended touch 2d point
+                secondPressPos = new Vector2(touch.position.x, touch.position.y);
+                torqueCurrent = torque;
+            }
+            //create vector from the two points
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+        }
+        return currentSwipe.x;
+    }
+}
---
+++ Assets/goalHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class goalHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static int score = 0;
+    public Text scoreText;
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + score.ToString("D3");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (other.tag == "ball")
+        {
+            Debug.Log("Won");
+            score++;
+        }
+    }
+}
